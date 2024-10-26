c23aace3f6d587cb8e2545b17ad14a2080eff65a
watanabe <watanabe-seiya441@g.ecc.u-tokyo.ac.jp>
2024-10-26 20:06:03 +0900

    aespaそれぞれを区別する動画再生コード

--- src/movie_detector_person.py
+++ src/movie_detector_person.py
@@ -1,14 +1,15 @@
+#### 4人の識別プログラム ####
+
 import cv2
 from ultralytics import YOLO
 import subprocess
 import os
 
-# video_path = "<input_movie_file_path>"
+
 video_path = "Whiplash.mp4"
-# output_path = "<output_movie_file_path>"
 output_path = "output.mp4"
 
-model = YOLO("yolov8n.pt")
+model = YOLO("best.pt")
 cap = cv2.VideoCapture(video_path)
 
 fps = cap.get(cv2.CAP_PROP_FPS)
@@ -21,7 +22,7 @@ out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
-        results = model.predict(frame, classes=[0])  # detect only person, class id is 0
+        results = model.predict(frame, classes=[0,1,2,3])  # detect only person, class id is 0,1,2,3
         annotated_frame = results[0].plot()
         out.write(annotated_frame)
         cv2.imshow("Frame", annotated_frame)
