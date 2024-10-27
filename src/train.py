41c7dc38ea34104efdb18eaf3bc45b4cde7bfbb1
watanabe <watanabe-seiya441@g.ecc.u-tokyo.ac.jp>
2024-10-27 10:14:00 +0900

    gitignore,train.pyのパスの修正

--- src/train.py
+++ src/train.py
@@ -2,4 +2,5 @@ from ultralytics import YOLO
 
 
 model = YOLO("yolov8n.pt")
-model.train(data="/Users/watanabeseiya/Desktop/Develop_開発/tk_2420/aespa_dataset/data.yaml", epochs=10, batch=20, device='mps')  # 作成したデータセット内のyamlファイルまでのパスを指定
+## batchが大きいと動かない
+model.train(data="/Users/watanabeseiya/Desktop/Develop_開発/tk_2420/aespa_dataset_watanabe/data.yaml", epochs=10, batch=20, device='mps')  # 作成したデータセット内のyamlファイルまでのパスを指定
